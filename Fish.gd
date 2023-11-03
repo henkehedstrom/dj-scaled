0f713ddff97ce736e1ec739f0369f3bdffcc8124
Henrik Hedström <henrik.hedstrom@hotmail.com>
2023-11-03 22:53:24 +0100

    first

---
+++ Fish.gd
@@ -0,0 +1,15 @@
+extends CharacterBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _physics_process(delta):
+	move_and_collide(Vector3(0,0,-0.01))
+	
