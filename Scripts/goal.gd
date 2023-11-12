1ed6516494a326dd7a143e63e02d9bb2dac58845
Henrik Hedström <henrik.hedstrom@hotmail.com>
2023-11-12 16:29:22 +0100

    *goalmanager and some logic for going into goals in order

--- Scripts/goal.gd
+++ Scripts/goal.gd
@@ -1,14 +1,32 @@
 extends Node3D
 
-@export var goal_text :String
+@export var goal_number :String
 @onready var pling = $AudioStreamPlayer3D
+var current_goal = false
+@onready var text_object = $Label3D
+
+var goal_manager 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var text_object = $Label3D
-	text_object.text = goal_text
+	text_object.text = goal_number
+	text_object.set_modulate(Color(1,0,0))
+
+func set_goalmanager(manager):
+	goal_manager = manager
+
+func set_current():	
+	current_goal = true
+	text_object.set_modulate(Color(0,1,0))
 	
+
+
 func _on_area_3d_body_entered(body):
-	pling.play()
-	print("entered")
-	pass # Replace with function body.
+	if body is CharacterBody3D: #TODO better check for player?
+		if current_goal:
+			pling.play()
+			print("entered" + self.name)
+			text_object.set_modulate(Color(0,0,1))
+			goal_manager.next()
+		
+
---
+++ Scripts/goal_manager.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+@onready var goals: Array = get_children()
+var currentIndex: int = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(goals)
+	goals[0].set_current()
+	for goal in goals:
+		goal.set_goalmanager(self)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func next():
+	if currentIndex + 1 < goals.size():
+		currentIndex+= 1
+		goals[currentIndex].set_current()
