ca293d3f54175dbc1a2a494563257c1493d2e229
Henrik Hedström <henrik.hedstrom@hotmail.com>
2023-11-12 23:37:01 +0100

    *added non functional options menu *added some back logic

--- Scenes/menu.gd
+++ Scenes/menu.gd
@@ -5,13 +5,21 @@ var player_info = {"name": "Name", "id": -1}
 
 signal player_connected(peer_id, player_info)
 
+@onready var main_menu :Control = $MainMenuControl
+@onready var multiplayer_menu: Control = $MultiplayerControl
+@onready var options_menu: Control  = $OptionsControl
+@onready var lobby_menu: Control = $LobbyControl
+
+var back = null
+var current_menu = main_menu
+@onready var quit_or_back_button = $QuitOrBackGameButton
+
+
+
 func _ready():
 	$Camera3D.current = true
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	$Control.visible = true
-	$MultiplayerControl.visible = false
-	$LobbyControl.visible = false
+	_show_menu(main_menu)
 	pass
 
 
@@ -20,7 +28,9 @@ func _on_solo_play_button_pressed():
 
 
 func _on_quit_game_button_pressed():
-	_quit_game()
+	_show_menu(back)
+	if back == null:	
+		_quit_game()
 
 
 func _change_scene(change_to_scene : PackedScene):
@@ -38,8 +48,7 @@ func _quit_game():
 
 
 func _on_multiplayer_button_pressed():
-	$Control.visible = false
-	$MultiplayerControl.visible = true
+	_show_menu(multiplayer_menu)
 
 
 func _on_host_game_button_pressed():
@@ -50,8 +59,7 @@ func _on_host_game_button_pressed():
 	
 	multiplayer.multiplayer_peer = peer
 	print("started server")
-	$MultiplayerControl.visible = false
-	$LobbyControl.visible = true
+	_show_menu(lobby_menu)
 
 
 func _on_join_game_button_pressed():
@@ -71,9 +79,7 @@ func _on_join_game_button_pressed():
 	print("started client")
 	multiplayer.peer_connected.connect(_on_player_connected)
 	
-	$MultiplayerControl.visible = false
-	$LobbyControl.visible = true
-	$LobbyControl/StartGameButton.visible = false
+	_show_menu(lobby_menu)
 
 func _on_player_connected(id : int):
 	_register_player.rpc_id(id, player_info)
@@ -95,3 +101,25 @@ func _register_player(new_player_info):
 	
 	if multiplayer.get_unique_id() == 1:
 		_host_announce.rpc_id(new_player_id)
+
+
+func _on_options_button_pressed():
+	_show_menu(options_menu)
+
+	
+func _show_menu(menu:Control):
+	back = current_menu #TODO: This does not always work, you can get stuck in a loop :)
+	current_menu = menu
+	_make_all_menus_invisible()
+	if menu == main_menu:
+		quit_or_back_button.text = "Quit"
+	else:
+		quit_or_back_button.text = "Back"
+	menu.visible = true
+
+func _make_all_menus_invisible():
+	main_menu.visible = false
+	multiplayer_menu.visible = false
+	options_menu.visible = false
+	lobby_menu.visible = false
+	
