f74a293f4f497419aa94308bdbb554a4ba021565
Henrik Hedström <henrik.hedstrom@hotmail.com>
2023-11-28 20:55:36 +0100

    *added timer

---
+++ RaceTimer.gd
@@ -0,0 +1,38 @@
+extends Node
+
+var current_time:float 
+var _is_started:bool
+@export var goal_manager :Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if goal_manager:
+		goal_manager.win.connect(on_win)
+	else:
+		print("GOAL MANAGER DOES NOT EXIST!")
+	reset()
+	start()
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if _is_started:
+		current_time += delta
+	pass
+	
+func on_win(id:int):
+	pause()
+	print("Player with id cleared the track in " + str(current_time) + " seconds!")
+	
+	
+
+func start():
+	_is_started = true
+
+func pause():
+	_is_started = false
+
+func reset():
+	current_time = 0.0
